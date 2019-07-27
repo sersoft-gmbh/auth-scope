33b30c6d5af0a850164099889669c15e33b21eef
Florian Friedrich <ffried@me.com>
2019-07-27 20:32:31 +0200

    Add Documentation

---
+++ Xcode/build/AuthScope.build/Release-iphoneos/AuthScope.build/DerivedSources/AuthScope_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char AuthScopeVersionString[];
+ extern const double AuthScopeVersionNumber;
+
+ const unsigned char AuthScopeVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:AuthScope  PROJECT:AuthScope-1" "\n";
+ const double AuthScopeVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ Xcode/build/AuthScope.build/Release-iphoneos/AuthScope.build/Objects-normal/arm64/AuthScope_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/florianfriedrich/Documents/ser.soft/Projects/AuthScope/Xcode/build/AuthScope.build/Release-iphoneos/AuthScope.build/DerivedSources/AuthScope_vers.c
