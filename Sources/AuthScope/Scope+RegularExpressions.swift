3b784e0ac6b06eb17d20ca2ab17bb22b857ae279
Florian Friedrich <ffried@me.com>
2022-09-15 08:37:14 +0200

    Add Swift 5.7 support

--- Sources/AuthScope/Scope+RegularExpressions.swift
+++ Sources/AuthScope/Scope+RegularExpressions.swift
@@ -1,18 +1,18 @@
 extension Scope {
     @inline(__always)
-    private var accessRangesRegExGroup: String {
+    private var accessRangesRegExGroupString: String {
         "(?:" + accessRanges.lazy.map(\.rawValue).joined(separator: "|") + ")"
     }
 
     @inline(__always)
-    private var accessRangesAsRegExGroups: Repeated<String> {
-        repeatElement(accessRangesRegExGroup, count: accessRanges.count)
+    private var accessRangesAsRegExGroupStrings: Repeated<String> {
+        repeatElement(accessRangesRegExGroupString, count: accessRanges.count)
     }
 
     /// Returns a regular expression string that matches a string that has exactly the access ranges in self (not more, not less).
     /// - Note: The order of the access ranges in the string is irrelevant.
     public var exactMatchRegExp: String {
-        "^" + accessRangesAsRegExGroups.joined(separator: String(Scope.stringSeparator)) + "$"
+        "^" + accessRangesAsRegExGroupStrings.joined(separator: String(Scope.stringSeparator)) + "$"
     }
 
     /// Returns a regular expression string that matches a string that has at least the access ranges in self (but can have more).
@@ -20,13 +20,13 @@ extension Scope {
     public var containsAllRegExp: String {
         let anyScopeRegEx = "(?:[^ ]+ )*"
         return "^" + anyScopeRegEx
-            + accessRangesAsRegExGroups.joined(separator: String(Scope.stringSeparator) + anyScopeRegEx)
+            + accessRangesAsRegExGroupStrings.joined(separator: String(Scope.stringSeparator) + anyScopeRegEx)
             + "(?: (?:[^ ]+(?: |$))+|$)"
     }
 
     /// Returns a regular expression string that matches a string that has at least one of access ranges in self (but not all).
     /// - Note: The order of the access ranges in the string is irrelevant.
     public var containsAnyRegExp: String {
-        "(?:^| )" + accessRangesRegExGroup + "(?: |$)"
+        "(?:^| )" + accessRangesRegExGroupString + "(?: |$)"
     }
 }
